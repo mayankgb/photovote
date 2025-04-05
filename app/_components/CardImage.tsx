d4f50ad9120843105716fec22f11146729d38334
Mayank Agrawal <mayankagrawal@Mayanks-MacBook-Air.local>
2025-04-05 20:42:22 +0530

    adding some image optimisation

--- app/_components/CardImage.tsx
+++ app/_components/CardImage.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import mradul from "@/public/mradul.jpg"
 import sid from "@/public/sidhart.png"
+import prashant from "@/public/prashant.png"
 
 export function CardImage() {
     return (
@@ -10,7 +11,7 @@ export function CardImage() {
                 <Image src={mradul} alt='icon' className="bg-yellow-100 object-cover z-0 absolute rounded-lg md:h-full h-[60%] w-28 md:w-52 transform -rotate-12 left-1/2 -translate-x-[150%]">
                     {/* Left Card Content */}
                 </Image>
-                <Image src={"https://d12hk4zd0jmtng.cloudfront.net/path/ASHNEEL SONI"} unoptimized quality={100} width={20} height={20} alt='icon' className=" z-10 object-cover absolute rounded-lg md:h-full h-[60%] w-28 md:w-52 left-1/2 -translate-x-1/2 -translate-y-[8%]">
+                <Image src={prashant}  alt='icon' className=" z-10 object-cover absolute rounded-lg md:h-full h-[60%] w-28 md:w-52 left-1/2 -translate-x-1/2 -translate-y-[8%]">
                     {/* Middle Card Content */}
                 </Image>
                 <Image src={sid} alt='icon' className="bg-black object-cover z-0 absolute rounded-lg md:h-full h-[60%] w-28 md:w-52 transform rotate-12 left-1/2 translate-x-[50%]">
